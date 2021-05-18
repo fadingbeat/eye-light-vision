0a8c30b2e36fd8f01de09f32f22d9d304069d91e
fadingbeat <fadingbeat@gmail.com>
2021-05-18 13:56:09 +0200

    added dialog window for main part

--- src/components/MainWindow.jsx
+++ src/components/MainWindow.jsx
@@ -2,12 +2,23 @@ import React, { Component } from 'react';
 import cover from "../images/cover.webp";
 import "../styles/MainWindow.scss";
 import { Button } from "@progress/kendo-react-buttons";
+import { Window } from "@progress/kendo-react-dialogs";
 // import Exercises from "./Exercises";
 
 class MainWindow extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        visible: true,
+        isReziable: false,
+    };
+  }
   render() {
     return (
       <div className="main-container">
+        {this.state.visible && 
+        <Window title="Eye treatment with color therapy" isResizable={true} initialWidth={1024} initialHeight={768}></Window>
+        }
         <img src={cover} alt="Illustration of an eye lifting weights"/>
         <div className="btn-wrapper">
           <Button primary={true}>Exercises</Button>
