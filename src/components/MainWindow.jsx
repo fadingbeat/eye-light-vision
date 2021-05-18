f95bced0ecdbe5d39a3a24da0afb65ca660dbac7
fadingbeat <fadingbeat@gmail.com>
2021-05-18 14:40:49 +0200

    mvp of main window app

--- src/components/MainWindow.jsx
+++ src/components/MainWindow.jsx
@@ -11,21 +11,26 @@ class MainWindow extends Component {
     this.state = {
         visible: true,
         isReziable: false,
+        windowStage: "FULLSCREEN"
     };
   }
   render() {
     return (
       <div className="main-container">
         {this.state.visible && 
-        <Window title="Eye treatment with color therapy" isResizable={true} initialWidth={1024} initialHeight={768}></Window>
+          <Window 
+            title="Eye treatment with color therapy" 
+            stage={this.state.windowStage} 
+            resizable={this.state.isResizable} >
+            <img src={cover} alt="Illustration of an eye lifting weights"/>
+            <div className="btn-wrapper">
+              <Button primary={true}>Exercises</Button>
+              <Button primary={true}>Facts</Button>
+              <Button primary={true}>Settings</Button>
+              <Button primary={true}>Help</Button>
+            </div>
+          </Window>
         }
-        <img src={cover} alt="Illustration of an eye lifting weights"/>
-        <div className="btn-wrapper">
-          <Button primary={true}>Exercises</Button>
-          <Button primary={true}>Facts</Button>
-          <Button primary={true}>Settings</Button>
-          <Button primary={true}>Help</Button>
-        </div>
       </div>
     );
   }
